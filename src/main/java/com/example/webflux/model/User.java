89e830b68ab8d5f0e79be34ee3470bae3241723c
kangseungmin <rkdtmdals999@naver.com>
2024-03-07 10:04:47 +0900

    [feat] User Data class 생성

---
+++ src/main/java/com/example/webflux/model/User.java
@@ -0,0 +1,26 @@
+package com.example.webflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
