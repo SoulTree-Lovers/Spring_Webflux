321dff150b0c48b5620406ba28ac183165ece89c
kangseungmin <rkdtmdals999@naver.com>
2024-03-09 11:49:49 +0900

    [feat] UserUpdateRequest 생성

---
+++ src/main/java/com/example/webflux/controller/dto/UserUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.example.webflux.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserUpdateRequest {
+
+    private String name;
+
+    private String email;
+
+}
