15414e8af57ed298825ee8fe389c3b689e4dff24
kangseungmin <rkdtmdals999@naver.com>
2024-03-12 13:55:15 +0900

    [feat] PostResponse DTO 구현

---
+++ src/main/java/com/example/webflux/controller/dto/PostResponse.java
@@ -0,0 +1,16 @@
+package com.example.webflux.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostResponse {
+
+    private String id;
+
+    private String content;
+
+}
