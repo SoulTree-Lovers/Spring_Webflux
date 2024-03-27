222441d91a9611ed5c07bbd6c682875241eb09f5
kangseungmin <rkdtmdals999@naver.com>
2024-03-27 16:02:30 +0900

    [feat] 어노테이션 추가

--- src/main/java/com/example/webflux/model/User.java
+++ src/main/java/com/example/webflux/model/User.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
@@ -11,16 +15,20 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users") // table 연동
 public class User {
 
+    @Id
     private Long id;
 
     private String name;
 
     private String email;
 
+    @CreatedDate // 생성 시간 자동 기록
     private LocalDateTime createdAt;
 
+    @LastModifiedDate // 수정 시간 자동 기록
     private LocalDateTime updatedAt;
 
 }
