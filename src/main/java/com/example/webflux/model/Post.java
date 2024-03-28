7e54d1a23536b95eb45005a659cbbba9fe009845
kangseungmin <rkdtmdals999@naver.com>
2024-03-28 16:02:57 +0900

    [feat] Post Dto 생성

---
+++ src/main/java/com/example/webflux/model/Post.java
@@ -0,0 +1,41 @@
+package com.example.webflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    private Long id;
+
+    @Column(value = "user_id")
+    private Long userId;
+
+    private String title;
+
+    private String content;
+
+    @Column("created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column("updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+
+}
