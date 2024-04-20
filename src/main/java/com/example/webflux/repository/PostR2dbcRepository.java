b10333296e8b21fe73a75e06a441995a9d83e1b8
kangseungmin <rkdtmdals999@naver.com>
2024-04-21 00:54:32 +0900

    feat: PostR2dbcRepository에  PostCustomR2dbcRepositoryImpl 상속받음
    - findAllByUserId() 메소드 구현체 사용을 위함

--- src/main/java/com/example/webflux/repository/PostR2dbcRepository.java
+++ src/main/java/com/example/webflux/repository/PostR2dbcRepository.java
@@ -2,8 +2,9 @@ package com.example.webflux.repository;
 
 import com.example.webflux.model.Post;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
-public interface PostR2dbcRepository extends ReactiveCrudRepository<Post, Long> {
+public interface PostR2dbcRepository extends ReactiveCrudRepository<Post, Long>, PostCustomR2dbcRepository {
 
-
+    Flux<Post> findByUserId(Long id);
 }
